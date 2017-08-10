a07cdf29333b7311ed0eadfd41850b97a6b6e530
Cristian Morataya <cris.morataya@gmail.com>
2017-08-10 14:26:04 -0600

    Fixed the up movement

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
-onready var spaceShip = get_node("spr_spaceShip")
+const speed = 4
+#onready var spaceShip = get_node("spr_spaceShip")
+onready var spaceShip = get_node("kbdy_spaceShip")
+var direction = Vector2(0,0)
 
 func _ready():
 	print("started")
-	set_process(true)
+	set_fixed_process(true)
 
-func _process(delta):
+func _fixed_process(delta):
+	
 	if Input.is_action_pressed("ui_left"):
 		spaceShip.rotate(-0.05)
-		print("LEFT")
+		print("LEFT");
 	if Input.is_action_pressed("ui_right"):
 		spaceShip.rotate(0.05)
-		print("RIGHT")
+		print("RIGHT");
+	
+	if Input.is_action_pressed("ui_up"):
+		direction += Vector2(0,-1)
+	if Input.is_action_pressed("ui_down"):
+		direction += Vector2(0,1)
+		
+	spaceShip.move(direction * delta * speed)
