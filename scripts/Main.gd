bd35bdef92fc2e21989fa002e90595b430a3b998
Cristian Morataya <cris.morataya@gmail.com>
2017-08-10 14:45:08 -0600

    Add Fire sprite and logic Add source and image of propulsionFire; add logic to show and hide when the key is pressed

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 const speed = 4
 #onready var spaceShip = get_node("spr_spaceShip")
 onready var spaceShip = get_node("kbdy_spaceShip")
+onready var propulsionFire = get_node("kbdy_spaceShip/spr_spaceShip/spr_propulsionFire")
 var direction = Vector2(0,0)
 
 func _ready():
@@ -10,6 +11,7 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	propulsionFire.hide()
 	
 	if Input.is_action_pressed("ui_left"):
 		spaceShip.rotate(-0.05)
@@ -20,6 +22,7 @@ func _fixed_process(delta):
 	
 	if Input.is_action_pressed("ui_up"):
 		direction += Vector2(0,-1)
+		propulsionFire.show()
 	if Input.is_action_pressed("ui_down"):
 		direction += Vector2(0,1)
 		
